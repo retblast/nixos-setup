3a1e971eb990b03d36635575735a238185c1cfe3
retblast <retblast@proton.me>
2026-04-14 14:01:00 -0500

    Add fix for plasma lag. See NixOS/nixpkgs#126590 and NixOS/nixpkgs#126590
    Signed-off-by: retblast <retblast@proton.me>

--- modules/kde.nix
+++ modules/kde.nix
@@ -47,6 +47,10 @@ in
       systemPackages =
         with pkgs;
         [
+          # UI customization
+          python3Packages.kde-material-you-colors
+          klassy
+
           #  Extra KDE stuff
           kdePackages.filelight
           kdePackages.qtsvg
@@ -126,6 +130,59 @@ in
         KWIN_USE_OVERLAYS = "1";
       };
     };
+
+    # https://old.reddit.com/r/NixOS/comments/1pdtc3v/kde_plasma_is_slow_compared_to_any_other_distro/
+    # https://github.com/NixOS/nixpkgs/issues/126590#issuecomment-3194531220
+    nixpkgs.overlays = [
+      (final: prev: {
+        kdePackages = prev.kdePackages.overrideScope (
+          kdeFinal: kdePrev: {
+            plasma-workspace =
+              let
+                # the package we want to override
+                basePkg = kdePrev.plasma-workspace;
+                # a helper package that merges all the XDG_DATA_DIRS into a single directory
+                xdgdataPkg = final.stdenv.mkDerivation {
+                  name = "${basePkg.name}-xdgdata";
+                  buildInputs = [ basePkg ];
+                  dontUnpack = true;
+                  dontFixup = true;
+                  dontWrapQtApps = true;
+                  installPhase = ''
+                    mkdir -p $out/share
+                    ( IFS=:
+                      for DIR in $XDG_DATA_DIRS; do
+                        if [[ -d "$DIR" ]]; then
+                          ${prev.lib.getExe prev.lndir} -silent "$DIR" $out
+                        fi
+                      done
+                    )
+                  '';
+                };
+                # undo the XDG_DATA_DIRS injection that is usually done in the qt wrapper
+                # script and instead inject the path of the above helper package
+                derivedPkg = basePkg.overrideAttrs {
+                  preFixup = ''
+                    for index in "''${!qtWrapperArgs[@]}"; do
+                      if [[ ''${qtWrapperArgs[$((index+0))]} == "--prefix" ]] && [[ ''${qtWrapperArgs[$((index+1))]} == "XDG_DATA_DIRS" ]]; then
+                        unset -v "qtWrapperArgs[$((index+0))]"
+                        unset -v "qtWrapperArgs[$((index+1))]"
+                        unset -v "qtWrapperArgs[$((index+2))]"
+                        unset -v "qtWrapperArgs[$((index+3))]"
+                      fi
+                    done
+                    qtWrapperArgs=("''${qtWrapperArgs[@]}")
+                    qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "${xdgdataPkg}/share")
+                    qtWrapperArgs+=(--prefix XDG_DATA_DIRS : "$out/share")
+                  '';
+                };
+              in
+              derivedPkg;
+          }
+        );
+      })
+    ];
+
     # system.replaceDependencies.replacements = [
     # 	# https://bugs.kde.org/show_bug.cgi?id=479891#c114
     # 	{
